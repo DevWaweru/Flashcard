95b4882a792918e6b9f13d1853e05d39ef4208c5
Waweru <developer.waweru@gmail.com>
2018-03-22 17:15:24 +0300

    Complete Project

--- js/sripts.js
+++ js/sripts.js
@@ -1,19 +1,45 @@
 $(document).ready(function(){
+    var hidden = function ficha(){
+        $("#java").hide();
+        $("#opera").hide();
+        $("#var").hide();
+        $("#chainn").hide();
+        $("#boolea").hide();                
+    };
 
+    $(".java").on('click', function(){
+        hidden();
+        $("#java").show();
+    });
+    $(".opera").on('click', function(){
+        hidden();
+        $("#opera").show();
+    });
+    $(".var").on('click', function(){
+        hidden();
+        $("#var").show();
+    });
+    $(".chainn").on('click', function(){
+        hidden();
+        $("#chainn").show();
+    });
+    $(".boolea").on('click', function(){
+        hidden();
+        $("#boolea").show();
+    });
 });
 
+// function dispJava(){
+//     var javaScrerator(){
+//     var operators = document.getElementById("opera");
+//     if(operators.style.display === "none"){
+//         operators.style.display = "block";
+//     }else operators.style.display = "none";
+// }ipt = document.getElementById("myContents");
+//     if(javaScript.style.display === "block"){
+//         javaScript.style.display = "none";
+//     }else javaScript.style.display = "block";
 
-function dispJava(){
-    var javaScript = document.getElementById("myContents");
-    if(javaScript.style.display === "block"){
-        javaScript.style.display = "none";
-    }else javaScript.style.display = "block";
-
-}
-//Operators
-function dispOperator(){
-    var operators = document.getElementById("opera");
-    if(operators.style.display === "none"){
-        operators.style.display = "block";
-    }else operators.style.display = "none";
-}
+// }
+// //Operators
+// function dispOp
